eb901d7cbf6c2a613a094ef8e60abe11d1d7591e
uphayled <uphayled@gmail.com>
2015-04-17 08:57:38 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+require 'header.php';
+?>
+	<div id="main">
+		<u><h2>Welcome to Raaaadd Pizza!!</h2></u>
+		<img src="logo.png" id="imgpizzapasta"/>
+		<div id = "info">
+			<h4>Restaurant Hours:</h4>
+			<p>Monday to Thursday: 6AM to 2AM</p>
+			<p>Friday to Saturday: 6AM to 4AM</p>
+			<p>Sunday: 8AM to 1AM</p>
+			<h4>Delivery Hours:</h4>
+			<p>Monday to Thursday: 11AM to 2AM</p>
+			<p>Friday to Saturday: 11AM to 4AM</p>
+			<p>Sunday: 11AM to 11PM</p>
+			<h4>Contact Information:</h4>
+			<p>Phone #: 450-555-1212</p>
+			<p>Email: pizzapasta@fournotwogo.ca</p>
+			<br>
+		</div>
+		<nav class="newssec">
+			<u><h3>NEWS</h3></u>
+			<hr>
+			<h4>No longer hiring!</h4>
+			<p>After an arduous candidate screening, we have finally decided on our new hiring for this quarter. Thank you to all applicants, and there will be opportunities in the future! A great welcome to the team to Danny Roux, in the position of French Media Correspondant!</p>
+			<hr>
+			<h4>Clearing confusion</h4>
+			<p>We would like to remind everyone that while a certain pizzaria in the area has been having trouble with technical issues regarding animatronics, we are not in any danger of having the same problems at 4No2Go Pizza and Pasta. That was an isolated incident due to careless programming, not an industry-wide disaster. Besides, we don't even have animatronics at our restaurant.</p>
+			<hr>
+			<h4>New musical selection</h4>
+			<p>After receiving many complaints regarding the content of our usual musical selections for the restaurant, 4No2Go Pizza and Pasta has put together a new playlist for customers to enjoy. The new playlist primarily includes the artists Andrew Stein, Kevin MacLeod and Yoav Landau. We hope you enjoy the new selection!</p>
+			<hr>
+			<h4>Now hiring!</h4>
+			<p>As 4No2Go Pizza and Pasta grows, we need new talent to handle the increasing tasks. Anyone interested in a position is invited to contact pizzapasta@fournotwogo.ca, with the subject line "Employment" for more information.</p>
+			<hr>
+			<h4>New location!</h4>
+			<p>It has been a difficult first year for 4No2Go's foray into the Pizza and Pasta field. Finally, we are picking up steam and have come far enough to purchase a storefront on the boardwalk of St-Anne-de-Bellevue! Everyone is welcome to the opening day party on December 9th, where all items are 20% off!</p>
+			<hr>
+		</nav>
+	</div>
+	<footer>
+		 RAD Pizza and Pasta, website designed for Michael Cooney, John Abbott College Computer Science's Database I, Autumn 2014 semester
+	</footer>
+</body>
+</html>
